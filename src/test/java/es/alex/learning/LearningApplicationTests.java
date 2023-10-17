cb94882c9456a2dd4b7fd534edd51351144ffef7
agarciam <agmarchena@gmail.com>
2023-10-17 15:52:12 +0000

    readme comment and code start with initializr

---
+++ src/test/java/es/alex/learning/LearningApplicationTests.java
@@ -0,0 +1,13 @@
+package es.alex.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
