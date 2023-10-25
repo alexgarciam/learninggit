59d2df1cc565c9a108734c345b0bd285e89d154b
agarciam <agmarchena@gmail.com>
2023-10-25 16:28:26 +0000

    hello3 and hell4 added to First controller

--- src/main/java/es/alex/learning/controllers/FirstController.java
+++ src/main/java/es/alex/learning/controllers/FirstController.java
@@ -4,6 +4,9 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,4 +22,18 @@ public class FirstController {
 	public List<String> getUsers() {
 		return Arrays.asList("Alex","Manuel","Benzema","paco");
 	}
+	
+	@GetMapping("/hello3")
+	@ResponseBody
+	public String helloWorld3(@RequestParam() String name) {
+		return "hello "+name;
+	}
+	
+	@GetMapping("/hello4/{name}")
+	@ResponseBody
+	public String helloWorld4(@PathVariable String name) {
+		return "hello "+name;
+	}
+
+	
 }
